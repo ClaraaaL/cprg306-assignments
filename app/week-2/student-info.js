1e54fe44628e6564f108d563bf6f2872f5d71abd
ClaraaaL <babananacream@gmail.com>
2024-09-11 12:50:42 -0600

    week2
    week-2 asgn (add dtudentinfo) update main page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Clara Lee</h1>
+        <p><Link href="https://github.com/ClaraaaL">https://github.com/ClaraaaL</Link></p>
+      </main>
+    );
+  }
